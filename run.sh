b3b7d63022f80aea04a8bee3c176ef2cf0a31a66
homomorphic-nb <116266660+homomorphic-nb@users.noreply.github.com>
2022-10-25 16:37:32 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+python3 -c 'import NB_WMain; NB_WMain.car_encrypt()'
+python3 -c 'import NB_WMain; NB_WMain.car_check()'
+
+python3 -c 'import NB_WMain; NB_WMain.a1_encrypt()'
+python3 -c 'import NB_WMain; NB_WMain.a1_check()'
+
+python3 -c 'import NB_WMain; NB_WMain.a2_encrypt()'
+python3 -c 'import NB_WMain; NB_WMain.a2_check()'
+
+python3 -c 'import NB_WMain; NB_WMain.cancer_encrypt()'
+python3 -c 'import NB_WMain; NB_WMain.cancer_check()' 
