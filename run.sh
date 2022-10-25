cc576f092dd77f114786a16418f388c4cb2590d6
homomorphic-nb <116266660+homomorphic-nb@users.noreply.github.com>
2022-10-25 18:10:22 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
+echo "a1_train/inference"
 python3 -c 'import NB_WMain; NB_WMain.a1_train()'
-python3 -c 'import NB_WMain; NB_WMain.a1_inference()'
+python3 -c 'import NB_WMain; NB_WMain.a1_inference()' 
 
+echo "a2_train/inference"
 python3 -c 'import NB_WMain; NB_WMain.a2_train()'
 python3 -c 'import NB_WMain; NB_WMain.a2_inference()'
 
+echo "cancer_train/inference"
 python3 -c 'import NB_WMain; NB_WMain.cancer_train()'
 python3 -c 'import NB_WMain; NB_WMain.cancer_inference()'
 
+echo "car_train/inference"
 python3 -c 'import NB_WMain; NB_WMain.car_train()'
-python3 -c 'import NB_WMain; NB_WMain.car_inference()' 
+python3 -c 'import NB_WMain; NB_WMain.car_inference()'
