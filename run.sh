e595e4c5429241e90b4a020670c2a0e8ef0b3145
homomorphic-nb <116266660+homomorphic-nb@users.noreply.github.com>
2022-10-25 17:05:43 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-python3 -c 'import NB_WMain; NB_WMain.car_encrypt()'
-python3 -c 'import NB_WMain; NB_WMain.car_check()'
+python3 -c 'import NB_WMain; NB_WMain.a1_train()'
+python3 -c 'import NB_WMain; NB_WMain.a1_inference()'
 
-python3 -c 'import NB_WMain; NB_WMain.a1_encrypt()'
-python3 -c 'import NB_WMain; NB_WMain.a1_check()'
+python3 -c 'import NB_WMain; NB_WMain.a2_train()'
+python3 -c 'import NB_WMain; NB_WMain.a2_inference()'
 
-python3 -c 'import NB_WMain; NB_WMain.a2_encrypt()'
-python3 -c 'import NB_WMain; NB_WMain.a2_check()'
+python3 -c 'import NB_WMain; NB_WMain.cancer_train()'
+python3 -c 'import NB_WMain; NB_WMain.cancer_inference()'
 
-python3 -c 'import NB_WMain; NB_WMain.cancer_encrypt()'
-python3 -c 'import NB_WMain; NB_WMain.cancer_check()' 
+python3 -c 'import NB_WMain; NB_WMain.car_train()'
+python3 -c 'import NB_WMain; NB_WMain.car_inference()' 
